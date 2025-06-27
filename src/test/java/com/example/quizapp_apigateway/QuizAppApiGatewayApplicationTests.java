d3cbf309a32bea5cfb56df8a11151a06abdb951b
Tejashwa2810 <tejashwa2810@gmail.com>
2025-06-27 10:40:11 +0530

    Initial commit

---
+++ src/test/java/com/example/quizapp_apigateway/QuizAppApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quizapp_apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
